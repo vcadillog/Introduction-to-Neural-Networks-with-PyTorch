91c852bf862529d9b1b06f1baf85dbf4ef786c0e
Victor Cadillo <45715531+vcadillog@users.noreply.github.com>
2023-03-05 15:32:25 -0500

    Update learning.py

--- utils/learning.py
+++ utils/learning.py
@@ -1,7 +1,7 @@
 import torch as th
 
 class learning_loop():
-    def __init__(self, N_EPOCH,model,device,train_data,val_data,test_data,optimizer,criterion,writer, mode ) -> None:
+    def __init__(self, N_EPOCH,model,device,train_data,val_data,test_data,optimizer,criterion, directory,mode ) -> None:
         self.N_EPOCH = N_EPOCH
         self.model = model
         self.device = device
@@ -9,10 +9,10 @@ class learning_loop():
         self.val_data = val_data
         self.test_data = test_data
         self.optimizer = optimizer
-        self.criterion = criterion
-        self.writer = writer
+        self.criterion = criterion        
         self.mode = mode     
-   
+        self.writer1=SummaryWriter('logs/'+directory,flush_secs=60)
+        self.writer2=SummaryWriter('logs/'+directory,flush_secs=60)   
 
     def train(self):
         for epoch in range(1, self.N_EPOCH+1):
@@ -21,15 +21,10 @@ class learning_loop():
                 X_train_ts, y_train_ts = train
                 X_val_ts, y_val_ts = test
 
-                # X_train_ts, y_train_ts = X_train_ts.to(self.device), y_train_ts.type(th.LongTensor).to(self.device)
-                # X_val_ts, y_val_ts = X_val_ts.to(self.device), y_val_ts.type(th.LongTensor).to(self.device)
                 X_train_ts, y_train_ts = X_train_ts.to(self.device), y_train_ts.to(self.device)
                 X_val_ts, y_val_ts = X_val_ts.to(self.device), y_val_ts.to(self.device)
-                # y_train_ts = y_train_ts.view(-1,1)
-                # y_val_ts = y_val_ts.view(-1,1)
-                
+
                 out = self.model(X_train_ts.to(th.float32))      
-                # loss = criterion(out, y_train_ts.to(th.float32))
                 loss = self.criterion(out, y_train_ts)
                 
                 self.optimizer.zero_grad()
@@ -45,14 +40,13 @@ class learning_loop():
                     loss_val = self.criterion(out_val, y_val_ts)
                     if self.mode == 'classification':
                         acc_val = (th.argmax(out_val, dim=1) == y_val_ts).float().mean().item()
-
-            self.writer.add_scalar('Loss/test', loss_val.item(), epoch)  
-            self.writer.add_scalar('Loss/train', loss.item(), epoch)
-            
+               
+            self.writer1.add_scalar('Loss/train', loss.item(), epoch)
+            self.writer2.add_scalar('Loss/test', loss_val.item(), epoch)  
+                       
             if self.mode == 'classification':
-                self.writer.add_scalar('Accuracy/test', acc_val*100, epoch)
-                self.writer.add_scalar('Accuracy/train', acc*100 , epoch)
-                
+                self.writer1.add_scalar('Accuracy/train', acc*100 , epoch)
+                self.writer2.add_scalar('Accuracy/test', acc_val*100, epoch)                           
 
             if epoch % 20 == 0:
                     if self.mode == 'classification':
@@ -71,9 +65,7 @@ class learning_loop():
         
         for (i,test) in enumerate(self.test_data, 0):
             
-            X_test_ts, y_test_ts = test
-            
-            # X_test_ts, y_test_ts = X_test_ts.to(self.device), y_test_ts.type(th.LongTensor).to(self.device)
+            X_test_ts, y_test_ts = test           
             X_test_ts, y_test_ts = X_test_ts.to(self.device), y_test_ts.to(self.device)
 
             
